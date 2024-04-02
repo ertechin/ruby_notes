a9c2892f78aeb2b4311d4329d2b392214bd73a81
ertechin <erselertekin@hotmail.com>
2024-04-02 22:24:34 +0300

    add basics file

---
+++ basics.rb
@@ -0,0 +1,15 @@
+
+# print: The print method outputs the given value(s) to the console without adding a line break at the end.
+# It simply displays the value(s) as they are.
+print "hello ruby!"
+
+# puts: The puts method also outputs the given value(s) to the console, but it adds a line break after each value.
+# It automatically moves the cursor to the next line after displaying the value(s).
+puts "hello ruby!", 3, 4, 5
+
+# p: The p method is similar to puts, but it provides a more detailed output.
+# It displays the value(s) as they are, but also includes the data type and any special characters.
+p "hello ruby!", 3, 4, 5
+
+# PEMDAS
+# Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
